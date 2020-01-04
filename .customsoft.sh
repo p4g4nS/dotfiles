47a13b806c7dfa711cf33352108de2833a0b0b7d
Ivan Davidov <davidov.ivan@gmail.com>
2020-01-04 16:38:04 +0100

    Added preinstall script.

--- .customsoft.sh
+++ .customsoft.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
+# vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
---
+++ .prepareinstall.sh
@@ -0,0 +1,7 @@
+#~/usr/bin/bash
+
+# set up folders
+mkdir -p ~/.config/nvim
+
+
+# copy configuration files
